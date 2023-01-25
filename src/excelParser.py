f06c0dc54d41c57fbc66abe900852ff646d2329d
unknown <aqdas_khan@hotmail.co.uk>
2023-01-25 04:29:22 +0000

    added xlwings. tryna fix it with pywin32 - lookin grim atm th0

--- src/excelParser.py
+++ src/excelParser.py
@@ -1,22 +1,27 @@
 # from Server.BaseServer import Server for subclasses
-import pandas as pd
-
+#import pandas as pd
+#import openpyxl
+import xlwings as xw
 
 class ExcelParser:
     # constructor. so object can just exist, then you insert values easily.
     def __init__(self):
-        self.dataFrame = None
+        self.sheet = ""
 
-    
     def pickFile(self, fileLocation):
         # error checking?
-        self.dataFrame = pd.read_excel(fileLocation)
+        sheet = xw.Book(fileLocation).sheets["Sheet1"]
+
+    def requireColumns(self, c1, c2):
+        pass
 
     def printFile(self):
-        print(self.dataFrame)
+        r = sheet.range("A1:A25").value
+        c = sheet.range("F5").value
+        print(r)
 
 # anything y000 p00t here is more or less for testing this crap out
 ep = ExcelParser()
-#ep.pickFile("C:\Users\deck\Documents\Git\GitProjects\billgraphs\data\For_aduquaas.xlsx")
-#ep.printFile()
+ep.pickFile(r"..\data\For_aduquaas.xlsx")
+ep.printFile()
 
